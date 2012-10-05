cc3229c5f597735b261de45c4192f59b29849ad4
Roland Baldovino <junebaldovino@gmail.com>
2012-10-05 16:49:23 +0800

    Placed reference links in README. Added idle timers to the plugin. FInished the next and previous functions. Added the interval for auto playing.

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # [Responsive Slideshow](http://junebaldovino.com)
+A responsive jquery slideshow with touch and idle time features.
 
-A div mask slider.
+##Features
+This slideshow uses [mattbryson / TouchSwipe-Jquery-Plugin](https://github.com/mattbryson/TouchSwipe-Jquery-Plugin) and [Paul Irish's jquery-idletimer plugin](https://github.com/paulirish/jquery-idletimer/).
 
+
+###Other Details
 * Source: [https://github.com/junebaldovino](hhttps://github.com/junebaldovino/jquery.resss.plugin)
 * Homepage: [http://junebaldovino.com](http://junebaldovino.com)
 * Twitter: [@wtfroland](http://twitter.com/wtfroland)
--- js/jquery.resss.plugin.js
+++ js/jquery.resss.plugin.js
@@ -11,47 +11,55 @@
  */
 
 (function($){
-	var settings = {
-      interval:		'3000',
-      easing: 		'easeOutBack',
-      valign: 		'middle',
-      controls: 	false,
-      controlsCont: null,
-      totalPg:  	null,
-      allimg: 		null
-    };
-    var _this;
+	var _this,settings;
 	var methods = {
 		init : function( options ) {
 			return this.each(function(){
+
+				settings = $.extend( {
+			      interval:		3000,
+			      animspeed: 	1000,
+			      easing: 		'easeOutBack',
+			      valign: 		'middle',
+			      controls: 	false,
+			      autoplay: 	false,
+			      controlsCont: null,
+			      totalPg:  	null,
+			      allimgs: 		null,
+			      idletime: 	null,
+			      idlemode: 	false,
+			      curPg: 		0
+			    }, options);
 			 
-			 _this = $(this);
-			 var $this = $(this),
-			     data = $this.data('resss');
-				
-				// initial settings
-				settings.allimg = _this.find('img');
-				settings.totalPg = settings.allimg.length;
-				console.log(settings.controls);
+				 _this = $(this);
+				 var $this = $(this),
+				     data = $this.data('resss');
+					
+					// initial settings
+					settings.allimgs = _this.find('li');
+					settings.totalPg = settings.allimgs.length;
+					console.log(settings.controls);
 
-				if(settings.controls) methods.initControls();
+					if(settings.controls) methods.initControls();
 
-				methods.positionImages();
-			    methods.addListeners();
-			     
-			 if (!data) {
-			   $(this).data('resss', {
-			       target : $this
-			   });
-			 }
+					methods.positionImages();
+					methods.resetImages();
+				    methods.addListeners();
+				    methods.initTimers();
+				     
+				if (!data) {
+					$(this).data('resss', {
+						target : $this
+				  	});
+				}
 			});
 		},
 		initControls : function(){
-
 			var cc = $('<div class="controls" />');
 			_this.append(cc);
 			for (var i = 0; i < settings.totalPg; i++ ){
-				cc.append($('<div />'));
+				var d = i==0 ? $('<div class="active" />') : $('<div />');
+				cc.append(d);
 			}
 		},
 		addListeners : function(){
@@ -60,11 +68,37 @@
 
 			$(window).bind({ 'resize.resss': function(e){methods.reposition();} });
 		},
+		initTimers : function(){
+			if(settings.autoplay) {
+				methods.ssplay();
+			}else{
+				$.idleTimer(settings.interval);
+
+				$(document).bind("idle.idleTimer", function(){
+					settings.idlemode = true;
+					settings.idletime = setInterval(function(){methods.onIdle()},settings.interval);
+				});
+				$(document).bind("active.idleTimer", function(){
+					settings.idlemode = false;
+					clearInterval(settings.idletime);				
+				});
+			}
+		},
+		ssplay : function(){
+			if(settings.idletime != null) clearInterval(settings.idletime);
+			settings.idletime = setInterval(function(){methods.ssplay()},settings.interval);
+			methods.slideNext();
+		},
+		onIdle : function() {
+			methods.slideNext();
+		},
 		reposition : function( ) {
 			methods.positionImages();
+			if(settings.autoplay) methods.ssplay();
 		},
 		swipe : function(event, direction, distance, duration, fingerCount){
-			console.log(direction);
+			if(direction=='left') methods.slideNext();
+			if(direction=='right') methods.slidePrev();
 		},
 		positionImages : function(){
 			switch (settings.valign) {
@@ -72,9 +106,12 @@
 					console.log('top');
 					break;
 				case 'middle':
-					settings.allimg.each(function(i){
-						var t = ($(this).height() - _this.height()) / 2;
-						$(this).css({top:-t});
+					settings.allimgs.each(function(i){
+						var l = $(this);
+						l.css({display:'block'});
+						var t = (l.find('img').height() - _this.height()) / 2;
+						$(this).find('img').css({top:-t});
+						methods.resetImages();
 					});
 					break;
 				case 'bottom':
@@ -83,6 +120,30 @@
 				default:
 					console.log('default');
 			} 
+		},
+		slideNext : function(){
+			var pl = $(settings.allimgs[settings.curPg]);
+			++settings.curPg;
+			if(settings.curPg > settings.totalPg-1) settings.curPg = 0;
+
+			var l = $(settings.allimgs[settings.curPg]);
+			l.css({display:'block',opacity:0}).stop(true).animate({opacity:1},settings.animspeed,settings.easing);
+			pl.stop(true).animate({opacity:0},settings.animspeed,settings.easing,function(){pl.css({display:'none',opacity:0})});
+		},
+		slidePrev : function(){
+			var pl = $(settings.allimgs[settings.curPg]);
+			--settings.curPg;
+			if(settings.curPg < 0) settings.curPg = settings.totalPg-1;
+
+			var l = $(settings.allimgs[settings.curPg]);
+			l.css({display:'block',opacity:0}).stop(true).animate({opacity:1},settings.animspeed,settings.easing);
+			pl.stop(true).animate({opacity:0},settings.animspeed,settings.easing,function(){pl.css({display:'none',opacity:0})});
+		},
+		resetImages : function(){
+			$(settings.allimgs).each(function(i){
+				if(i != settings.curPg)$(settings.allimgs[i]).css({opacity:0,display:'none'});
+				else $(settings.allimgs[i]).css({opacity:1,display:'block'});
+			});
 		},
 		destroy : function( ) {
 
